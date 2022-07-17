b11ec7a5a058c6b0dd64ae5b5cd3fa8d719f4b55
Gaurav Pal <spare.slant@gmail.com>
2022-07-17 14:43:25 +0100

    added policy to allow service connector principal to read and write from source/destination

--- connector_hub/policy.tf
+++ connector_hub/policy.tf
@@ -3,6 +3,7 @@ resource "oci_identity_policy" "sc_policy" {
   description    = var.policy_description
   name           = var.policy_name
   statements = [
-    "Allow group ${var.group_name} to read metrics in compartment ${var.compartment_name}"
+    "allow any-user to read metrics in tenancy where all {request.principal.type = 'serviceconnector', request.principal.compartment.id = ${var.compartment_id}, target.compartment.id in (${var.compartment_id})}",
+    "allow any-user to manage objects in compartment id ${var.compartment_id} where all {request.principal.type='serviceconnector', target.bucket.name=${var.sc_target_bucket}, request.principal.compartment.id=${var.compartment_id}}"
   ]
 }
